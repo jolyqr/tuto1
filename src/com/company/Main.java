9d8b246be2122c1017c9bb3b426bce440b6dcb3b
jolyqr <jolyqr@gmail.com>
2020-07-08 23:36:59 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test 1 : création d'un objet
+        Personne object1=new Personne();
+        object1.age=18;
+        object1.demenage();
+    }
+}
